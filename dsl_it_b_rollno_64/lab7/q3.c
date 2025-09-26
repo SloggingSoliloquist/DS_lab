ca413ac2961b2225e6648c1d8ae95401cce915f5
SloggingSoliloquist <raopiano20@gmail.com>
2025-09-26 13:43:34 +0530

    Update q3.c

--- dsl_it_b_rollno_64/lab7/q3.c
+++ dsl_it_b_rollno_64/lab7/q3.c
@@ -1,164 +1,98 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-struct stk{
-char val;
-struct stk *next;
-};
-struct output{
-char val;
-struct output *next;
-};
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>   // for isalnum()
+#include <string.h>  // for strlen()
 
-void attach_node_to_op(struct output **node, char c)
-{
-    if(*node==NULL)
-            {
-                *node= (struct output*)malloc(sizeof(struct output));
-                (*node)->val =c;
-                (*node)->next= NULL;
-            }
-            else
-            {
-                struct output *new_node = (struct output*)malloc(sizeof(struct output));
-                new_node->val = c;
-                new_node->next = NULL;
-                (*node)->next = new_node;
-            }
+// Node structure for stack
+typedef struct Node {
+    char data;
+    struct Node* next;
+} Node;
+
+Node* top = NULL;
+
+// Push onto stack
+void push(char c) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (!newNode) {
+        printf("Heap overflow\n");
+        exit(1);
+    }
+    newNode->data = c;
+    newNode->next = top;
+    top = newNode;
 }
-void attach_node_to_stk(struct stk **head, char c)
-{
-    if(*head==NULL)
-            {
-                *head= (struct stk*)malloc(sizeof(struct stk));
-                (*head)->val =c;
-                (*head)->next= NULL;
-            }
-            else
-            {
-                struct stk *current = (struct stk*)malloc(sizeof(struct stk));
-                current->val = c;
-                current->next = *head;
-                *head = current;
-            }
+
+// Pop from stack
+char pop() {
+    if (top == NULL) {
+        return -1;
+    }
+    Node* temp = top;
+    char popped = temp->data;
+    top = top->next;
+    free(temp);
+    return popped;
 }
-int is_operator(char c)
-{
-    char operators[5] = "+-*/^";
-    for(int i = 0; i<5; i++)
-    {
-        if (c==operators[i])
-        {
-            return 1;
+
+// Peek at stack top
+char peek() {
+    if (top == NULL) return -1;
+    return top->data;
+}
+
+// Check precedence
+int precedence(char op) {
+    switch (op) {
+        case '^': return 3;
+        case '*': 
+        case '/': return 2;
+        case '+': 
+        case '-': return 1;
+        default: return 0;
+    }
+}
+
+int main() {
+    char infix[100], postfix[100];
+    int i, j = 0;
+    char token, x;
+
+    printf("Enter an infix expression: ");
+    scanf("%s", infix);
+
+    for (i = 0; i < strlen(infix); i++) {
+        token = infix[i];
+
+        if (isalnum(token)) {
+            // Operand → directly to output
+            postfix[j++] = token;
+        } 
+        else if (token == '(') {
+            push(token);
+        } 
+        else if (token == ')') {
+            while ((x = pop()) != '(') {
+                postfix[j++] = x;
+            }
+        } 
+        else { 
+            // Operator
+            while (top != NULL && precedence(peek()) >= precedence(token)) {
+                postfix[j++] = pop();
+            }
+            push(token);
         }
     }
+
+    // Pop remaining operators
+    while (top != NULL) {
+        postfix[j++] = pop();
+    }
+
+    postfix[j] = '\0';
+
+    printf("Postfix expression: %s\n", postfix);
+
     return 0;
-}
-int is_operand(char c)
-{
-    return((c>='A'&&c<='Z')||(c>='a'&&c<='z')||(c>='0'&&c<='9'));
-}
-int precedence(char c)
-{
-    if(c=='+' || c=='-')
-    {
-        return 1;
-    }
-    else if(c=='*' || c=='/')
-    {
-        return 2;
-    }
-    else if(c=='^')
-    {
-        return 3;
-    }
-}
-
-int has_higher_or_equal_precedence(char c1, char c2)
-{
-    int p1 = precedence(c1);
-    int p2 = precedence(c2);
-    if(p1==p2)
-    {
-        if(c1=='^')
-        {
-            return 0;
-        }
-        return 1;
-    }
-    return (p1>p2);
-}
-
-void convert_to_postfix(char infix[100], char str[100], struct stk **head_stk)
-{
-    int i = 0, j = 0;
-
-    strcat(infix, ")");
-    *head_stk = (struct stk*)malloc(sizeof(struct stk));
-    (*head_stk)->val= '(';
-    (*head_stk)->next= NULL;
-    struct stk* current_stk = (*head_stk);
-    char c=infix[0];
-    while(infix[i]!='\0')
-    {
-        if (is_operand(c)==1)
-        {
-            printf("no");
-            printf("%c", c);
-            str[j++]=c;
-
-        }
-        else if(c=='(')
-        {
-            attach_node_to_stk(head_stk, c);
-        }
-        else if(is_operator(c))
-        {
-            while(*head_stk!=NULL && has_higher_or_equal_precedence((*head_stk)->val, c))
-                  {
-                        str[j++]=(*head_stk)->val;
-                        struct stk *temp = (*head_stk);
-                        *head_stk = (*head_stk)->next;
-                        free(temp);
-                  }
-                  attach_node_to_stk(head_stk, c);
-        }
-        else if(c==')')
-        {
-            while(*head_stk!=NULL && (*head_stk)->val!='(')
-                  {
-                        str[j++]=(*head_stk)->val;
-                        struct stk *temp = (*head_stk);
-                        *head_stk = (*head_stk)->next;
-                        free(temp);
-                  }
-                        struct stk *temp = (*head_stk);
-                        *head_stk = (*head_stk)->next;
-                        free(temp);
-        }
-        i++;
-        c= infix[i];
-    }
-    printf("The evaluated expression: %s", str);
-}
-
-void display_ll(struct output *head)
-{
-    struct output *current = head;
-    while(current!=NULL)
-    {
-        printf("%d->", current->val);
-    }
-    printf("NULL");
-}
-int main()
-{
-    char infix[100];
-    char postfix[100];
-    struct stk *head_stk =NULL;
-    struct output *head_op =NULL;
-    printf("Enter the expression you want to evaluate: ");
-    scanf("%[^\n]", infix);
-    convert_to_postfix(infix, postfix, &head_stk);
-return 0;
 }
